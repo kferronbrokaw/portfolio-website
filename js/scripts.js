07a697bf22d2fdae00f7699c690eb73689549153
kferronbrokaw <kferron@brokaw.com>
2022-10-18 22:07:39 -0400

    revise for error fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
-console.log("Hello there")
+console.log('Hello there')
 
-
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
